6d9fb2f2e34e0031b7972b6767c5ab9d9fc66df9
Ankit Kumar <48655921+Ankit687@users.noreply.github.com>
2020-10-28 11:33:03 +0530

    done

--- Ticket.java
+++ Ticket.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
-
 import javax.swing.*;
 public class Ticket extends JFrame implements ActionListener
 {
